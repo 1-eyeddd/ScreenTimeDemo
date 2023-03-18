dfddc1c90779111809e019f45f85da5a3194462d
1-eyeddd <seisyuu43@icloud.com>
2023-03-18 15:16:04 +0900

    Initial Commit

---
+++ ScreenTimeDemo/ScreenTimeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeDemoApp.swift
+//  ScreenTimeDemo
+//
+//  Created by Tsubata Seisyu on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
